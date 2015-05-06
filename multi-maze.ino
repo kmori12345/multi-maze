e973de2804acf4756133aea13d59cf674dc25a68
kmori12345 <kmori15@punahou.edu>
2015-05-05 14:26:03 -1000

    In this class I drew the first maze and added a character.
    I just used the DrawPx statement to build the maze, the character, and
    the portal to the next maze

--- multi-maze.ino
+++ multi-maze.ino
@@ -1,9 +1,84 @@
+#include <MeggyJrSimple.h> 
+
+
+
 void setup() {
   // put your setup code here, to run once:
+  MeggyJrSimpleSetup();
+  
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{
+  drawDot(); 
+  drawPortal();
+  Maze1();
+  DisplaySlate();  
+  ClearSlate();
 }
+
+void drawDot()
+{
+ DrawPx(0,7,Blue);
+}
+
+void UpdateDot()
+{
+  
+}
+
+
+void drawPortal()
+{
+  DrawPx(1,0,4);
+}
+
+void Maze1()
+{
+  DrawPx(0,0,1);
+  DrawPx(0,1,1);
+  DrawPx(0,2,1);
+  DrawPx(0,3,1);
+  DrawPx(1,2,1);
+  DrawPx(1,3,1);
+  DrawPx(1,5,1);
+  DrawPx(1,6,1);
+  DrawPx(1,7,1);
+  DrawPx(2,0,1);
+  DrawPx(2,2,1);
+  DrawPx(2,3,1);
+  DrawPx(2,5,1);
+  DrawPx(2,6,1);
+  DrawPx(2,7,1);
+  DrawPx(3,0,1);
+  DrawPx(3,2,1);
+  DrawPx(3,3,1);
+  DrawPx(3,7,1);
+  DrawPx(4,0,1);
+  DrawPx(4,2,1);
+  DrawPx(4,3,1);
+  DrawPx(4,4,1);
+  DrawPx(4,5,1);
+  DrawPx(4,7,1);
+  DrawPx(5,0,1);
+  DrawPx(5,2,1);
+  DrawPx(5,3,1);
+  DrawPx(5,4,1);
+  DrawPx(5,5,1);
+  DrawPx(5,7,1);
+  DrawPx(6,0,1);
+  DrawPx(6,7,1);
+  DrawPx(7,0,1);
+  DrawPx(7,1,1);
+  DrawPx(7,2,1);
+  DrawPx(7,3,1);
+  DrawPx(7,4,1);
+  DrawPx(7,5,1);
+  DrawPx(7,6,1);
+  DrawPx(7,7,1);
+}
+
+
+  
+  
