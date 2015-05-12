a83d50d0f9588d784ef76903bfbb566f1d410fad
kmori12345 <kmori15@punahou.edu>
2015-05-11 14:21:15 -1000

    Got the player moving correctly, maze2
    I got my player to move on it’s own without being glitchy.  I also
    Built in a counter to separate how fast the dot movies and when you con
    press the buttons.  I also started my second maze and made it so if the
    dot touches the wall it returns back to the start.

--- multi-maze.ino
+++ multi-maze.ino
@@ -5,6 +5,8 @@ int y=7;          //player y coordinate
 int portalx=0;    //portal x coordinate
 int portaly=0; //portal y coordinate
 int direction=180;
+int counter=0;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,26 +16,47 @@ void setup() {
 
 void loop() 
 {
+  if (counter>9999)
+  {
+    counter=0;
+  }
+  else counter++;
   drawPortal1();
   Maze1();
   drawDot();
-  UpdateDot();
+  if (counter %250 == 0)
+  {
+    UpdateDot();
+  }  
+  if (ReadPx(x,y) == Red)      //returns true if there's a match
+    {
+      x=0;
+      y=7;
+    }
+
   DisplaySlate(); 
-  delay(1000); 
   ClearSlate();
   CheckButtonsPress();
     if (Button_Right)
-      direction = 90;    //check buttons
-                       //moves right
+    {
+      direction = 90;
+    }    
+                      
     if (Button_Left)
+    {
       direction = 270;
+    }
       
-    if (Button_Down)   
-      direction = 180;    //moves down
+    if (Button_Down)
+    {   
+      direction = 180;  
+    }
       
     if (Button_Up) 
+    {
       direction = 0;
-  UpdateDot();    //moves up
+    }
+   //moves up
       
   
 }
@@ -73,6 +96,10 @@ void UpdateDot()
 void drawPortal1()
 {
   DrawPx(1,0,4);
+}
+void drawPortal2()
+{
+  DrawPx(7,0,4);
 }
 
 void Maze1()
@@ -120,8 +147,49 @@ void Maze1()
   DrawPx(7,7,1);
 }
 
+void Maze2()
+{
+  DrawPx(0,0,1);
+  DrawPx(0,1,1);
+  DrawPx(0,2,1);
+  DrawPx(0,3,1);
+  DrawPx(0,4,1);
+  DrawPx(0,5,1);
+  DrawPx(0,6,1);
+  DrawPx(0,7,1);
+  DrawPx(1,0,1);
+  DrawPx(2,0,1);
+  DrawPx(2,2,1);
+  DrawPx(2,3,1);
+  DrawPx(2,4,1);
+  DrawPx(2,5,1);
+  DrawPx(2,6,1);
+  DrawPx(2,7,1);
+  DrawPx(3,0,1);
+  DrawPx(3,2,1);
+  DrawPx(3,3,1);
+  DrawPx(3,4,1);
+  DrawPx(3,5,1);
+  DrawPx(3,6,1);
+  DrawPx(3,7,1);
+  DrawPx(4,0,1);
+  DrawPx(5,0,1);
+  DrawPx(5,1,1);
+  DrawPx(5,2,1);
+  DrawPx(5,3,1);
+  DrawPx(5,4,1);
+  DrawPx(5,5,1);
+  DrawPx(5,6,1);
+  DrawPx(6,0,1);
+  DrawPx(6,1,1);
+  DrawPx(6,2,1);
+  DrawPx(6,3,1);
+  DrawPx(6,4,1);
+  DrawPx(6,5,1);
+  DrawPx(6,6,1);
+}
 
-  
+
   
   
   
